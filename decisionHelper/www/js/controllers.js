cb81e8223b83ebbb0c307677126355b5d8c1e52b
Attentatosch <sebastian-antosch@t-online.de>
2014-11-14 17:25:24 +0100

    Ionic working!

--- decisionHelper/www/js/controllers.js
+++ decisionHelper/www/js/controllers.js
@@ -5,4 +5,26 @@ angular.module('starter.controllers', [])
 })
 
 .controller('CoinflipCtrl', function($scope) {
+      $scope.should = true;
+      $scope.amount = 0;
+      $scope.roll = function()
+      {
+        $scope.amount = 10;
+        $scope.rotate(0);
+      };
+
+      $scope.rotate = function(i)
+      {
+        if(i<=$scope.amount)
+        {
+          $scope.should = !$scope.should;
+          window.setTimeout(function(){
+            $scope.rotate(i+1);
+          },1000);
+        }
+      };
+
+      $scope.roll();
+
+
 });
