03ca4e719739d71f9cbbc5f78fa6da76a4a5d80f
Attentatosch <sebastian-antosch@t-online.de>
2014-11-16 19:46:27 +0100

    Coinflip fertig gestellt!

--- decisionHelper/www/js/controllers.js
+++ decisionHelper/www/js/controllers.js
@@ -4,42 +4,65 @@ var app = angular.module('starter.controllers', [])
 app.controller('HomeCtrl', function($scope) {
 });
 
-app.controller('CoinflipCtrl', function($scope)
+/*
+ * Controller for coinflip view
+ * @param $scope - passd by angular
+ */
+app.controller('CoinflipCtrl', function($scope, $timeout)
 {
-    var min = 1;
-    var max = 3;
+    var min = 10;
+    var max = 31;
 
     $scope.should = true;
     $scope.amount = 0;
-    $scope.roll = function()
+
+    /*
+     * flips the coin
+     */
+    $scope.flip = function()
     {
         $scope.amount = Math.floor(Math.random()*(max-min+1)) + min;
         $scope.rotate(0);
     };
 
+    /*
+     * Reflips the coin
+     */
+    $scope.reflip = function()
+    {
+        $scope.$broadcast('scroll.refreshComplete');
+        $timeout($scope.flip, 800);
+        //$scope.flip();
+    };
+
+
+
+    /*
+     * Rotates the "coin" by one - is called recursively
+     * @param {number} i - the count of the recursive call
+     */
     $scope.rotate = function(i)
     {
         if(i <= $scope.amount)
         {
             var timeout = Math.ceil(500 * i / $scope.amount);
+            if(timeout < 100 ){timeout = 100};
 
             $scope.should = !$scope.should;
             $scope.$apply();
 
-            window.setTimeout(function(){
+            $timeout(function(){
                 $scope.rotate(i+1);
             },timeout);
         }
         else
         {
-
             $scope.amount = 0;
-
-            $scope.apply();
+            $scope.$apply();
         }
     };
 
-    $scope.roll();
+    $scope.flip();
 
 
 });
